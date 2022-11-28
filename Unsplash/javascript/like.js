6dacc2f08e3dcafaacf7c38e73b86adb15f17fb4
Wilczur <jakublelakowski@gmail.com>
2022-11-28 15:42:30 +0100

    Overlay i like button

--- Unsplash/javascript/like.js
+++ Unsplash/javascript/like.js
@@ -1,5 +1,11 @@
-
-
-like = () => {
-    
+let likinStat = 0;
+const like = () => {
+    console.log("xd")
+    if(likinStat == 0){
+        document.getElementById("imgLike").src = "img/like.png";
+        likinStat = 1;
+    } else {
+        document.getElementById("imgLike").src = "img/unlike.png";
+        likinStat = 0;
+    }
 }
