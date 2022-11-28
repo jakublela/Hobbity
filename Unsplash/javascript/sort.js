5906e58f1189ec418d5b390105ec4459cdc1eeae
Wilczur <jakublelakowski@gmail.com>
2022-11-28 12:57:36 +0100

    Update sort.js

--- Unsplash/javascript/sort.js
+++ Unsplash/javascript/sort.js
@@ -3,21 +3,29 @@ const authorization = new Headers();
 authorization.append('Authorization', "Client-ID zrpNTzftIorJiuJScfImsSR-K4dUG1ZPC9GDDzjBvao");
 
 function sortPhotos(){
-    sortedPhotos("photos/order_by/", 8);
+    sortedPhotos("photos/order_by?");
 }
 
-sortedPhotos = (urlEnd, num) => {
+sortedPhotos = (urlEnd) => {
     console.log(num);
     fetch("https://api.unsplash.com/" + urlEnd,{
         method: "GET",
-        headers: authorization,
-        params: {
-            count: num
-        }
+        headers: authorization
     })
     .then((response) => response.json())
     .then((photos) => {
         console.log(photos);
+        displayPhotos(photos, false);
+        photos.forEach(photo => {
+            let newDiv = document.createElement("div");
+            let newPhoto = document.createElement("img");
+            newPhoto.id = "photo";
+            newPhoto.src = photoUrl(photo, 250, 2);
+            let currentDiv = document.getElementById("ender");
+            newDiv.id = "photo_template";
+            newDiv.appendChild(newPhoto);
+            document.body.insertBefore(newDiv, currentDiv);
+        });
         
     })
     .catch((error) => console.log(error));  
